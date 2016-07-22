be69abc4ef97ec065399eb1f93a743ed3f0c0865
Chunlei Hou <omni360@qq.com>
2016-07-22 12:15:36 +0800

    refactor

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,7 +1,9 @@
-
-/// <reference path="threejs/three.d.ts" />
-/// <reference path="threejs/three-orbitcontrols.d.ts" />
-/// <reference path="threejs/ColladaLoader.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="jqueryui/jqueryui.d.ts" />
+/// <reference path="jstree/jstree.d.ts" />
 /// <reference path="purl/purl.d.ts" />
+/// <reference path="threejs/three-orbitcontrols.d.ts" />
+/// <reference path="threejs/three.d.ts" />
+/// <reference path="threejs/ColladaLoader.d.ts" />
+/// <reference path="threejs/three-transformcontrols.d.ts" />
+/// <reference path="threejs/three-orthographictrackballcontrols.d.ts" />
